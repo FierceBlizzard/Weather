584cf09740ce2a05904a95075bb9a064ac5e8366
FierceBlizzard <abhipandey2k1@gmail.com>
2022-09-22 13:39:47 -0400

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">Welcome</header>
+            <header className="App-header">Poke Weather</header>
         </div>
     );
 }
