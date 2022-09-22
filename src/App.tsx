7cb2d5d9be4505e9f47f576a234275331fe8d862
FierceBlizzard <abhipandey2k1@gmail.com>
2022-09-22 13:35:39 -0400

    get up all the packages and files to begin building

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">Welcome</header>
+        </div>
+    );
+}
+
+export default App;
